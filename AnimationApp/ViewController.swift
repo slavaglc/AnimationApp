5c41697b8b7ef5beb38b45b7b5d647f08b5de4a9
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-08-04 23:32:08 +0300

    added outlets and animation enums

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,15 +5,69 @@
 //  Created by slava on 04.08.2021.
 //
 
-import UIKit
+import Spring
+
+enum Animation: String, CaseIterable {
+    case shake = "shake"
+    case slideLeft = "slideLeft"
+    case pop = "pop"
+    case flipX = "flipX"
+    case flipY = "flipY"
+    case morph = "morph"
+    case squeeze = "squeeze"
+    case flash = "flash"
+    case wobble = "wobble"
+    case swing = "swing"
+}
+
+enum AnimationCurve: String, CaseIterable {
+    case easeIn = "easeIn"
+    case easeOut = "easeOut"
+    case easeInOut = "easeInOut"
+    case linear = "linear"
+    case spring = "spring"
+    case easeInSine = "easeInSine"
+    case easeOutSine = "easeOutSine"
+    case easeInOutSine = "easeInOutSine"
+    case easeInQuad = "easeInQuad"
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var springView: SpringView!
+    
+    let animations: [Animation] = [
+        .shake,
+        .slideLeft,
+        .pop,
+        .flipX,
+        .flipY,
+        .morph,
+        .squeeze,
+        .flash,
+        .wobble,
+        .swing
+    ]
+    
+    @IBAction func startAnimationBtn(_ sender: UIButton) {
+        setOptions()
+        springView.animate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    func setOptions() {
+        let animation = animations.randomElement()
+        
+        springView.force = 1
+        springView.delay = 0
+        springView.duration = 5
+        
+        springView.animation = animation?.rawValue ?? ""
+    }
 
 }
 
